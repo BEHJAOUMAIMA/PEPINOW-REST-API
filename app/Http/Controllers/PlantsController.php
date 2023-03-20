ab1247033bd9010e1361322628d1da9a5124ebdc
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2023-03-20 17:36:07 +0000

    add request for plants && create functions index + store

--- app/Http/Controllers/PlantsController.php
+++ app/Http/Controllers/PlantsController.php
@@ -1,10 +1,37 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Http\Requests\StorePlantsRequest;
+use App\Models\Plants;
 use Illuminate\Http\Request;
 
+/**
+ * Summary of PlantsController
+ */
 class PlantsController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $plants = Plants::with('category')->get();
+        return response()->json([
+            'status'=>true,
+            'songs'=>$plants
+        ]);
+
+    }
+
+
+    public function store(StorePlantsRequest $request)
+    {
+        $plant = Plants::create($request->all());
+        return response()->json([
+            'status'=>true,
+            'message'=>'Plant created Successfully !',
+            'song'=>$plant,
+        ],200);
+    }
+
 }
