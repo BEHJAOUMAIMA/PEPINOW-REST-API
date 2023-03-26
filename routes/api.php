bc480b577d8a89f46515cd15b72b321ad84e0057
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2023-03-26 11:45:08 +0000

    Définition des routes pour chaque middleware

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\PlantsController;
+use  App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -24,6 +26,22 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
     Route::post('/logout',[AuthController::class,'logout']);
     Route::put('/updateProfile',[AuthController::class,'updateProfile']);
+    Route::get('/allPlants', [PlantsController::class, "index"]);
+    Route::get('/plant/{id}', [PlantsController::class, "show"]);
+    Route::get('/plants/categories/{id}', [PlantsController::class, 'filterCategory']);
+});
+Route::middleware(['auth', 'IsAdmin'])->group(function () {
+    Route::apiResource('/categories', CategoryController::class);
+    Route::get('/plants', [PlantsController::class, "index"]);
+    Route::get('/getPlant/{id}', [PlantsController::class, "show"]);
+    Route::put('/editPlant/{id}', [PlantsController::class, "update"]);
+    Route::delete('/deletePlant/{id}', [PlantsController::class, "destroy"]);
+    Route::get('/allUsers',[AuthController::class,'allUsers']);
+    Route::put('/changeRole/{id}',[AuthController::class,'updateRole']);
+});
+
+Route::middleware(['auth', 'IsSeller'])->group(function () {
+    Route::apiResource('/plants', PlantsController::class);
 });
 
 Route::post('/auth/register', [AuthController::class, 'createUser']);
