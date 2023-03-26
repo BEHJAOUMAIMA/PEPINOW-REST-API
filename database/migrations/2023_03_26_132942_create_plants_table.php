1804a7a3dbf7f340c526c04f5c587774548d8c5c
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2023-03-26 14:31:52 +0000

    update migrations

--- database/migrations/2023_03_26_132942_create_plants_table.php
+++ database/migrations/2023_03_26_132942_create_plants_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->float('price');
             $table->string('image');
             $table->unsignedBigInteger('category_id');
-            $table->foreignId('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->foreign('category_id')->references('id')->on('categories')->cascadeOnDelete();
             $table->timestamps();
         });
     }
