79a404517fb602ef62facc02a319f497c8f41ef6
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2023-03-26 11:06:00 +0000

    ajout du methode change Role

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -19,37 +19,8 @@ class AuthController extends Controller
      *
      */
     public function createUser(Request $request){
-        // try {
-        //     $validateUser = Validator::make($request->all(), [
-        //         'name' => 'required|string',
-        //         'email'=>'required|email|unique:users,email',
-        //         'password'=>'required|string|confirmed|min:5'
-        //     ]);
-        //     if($validateUser->fails()){
-        //         return response()->json([
-        //             'status' => false,
-        //             'message' => 'Validation Error',
-        //             'errors' => $validateUser->errors()
-        //         ], 401);
-        //     }
-        //     $user = User::create([
-        //         'name'=>$request->name,
-        //         'email'=>$request->email,
-        //         'password'=>Hash::make($request->password)
-        //     ]);
 
-        //     return response()->json([
-        //         'status' => true,
-        //         'message'=> 'User created Successfully',
-        //         'token' => $user->createToken("API TOKEN")->plainTextToken
-        //     ], 200);
 
-        // } catch (\Throwable $th) {
-        //     return response()->json([
-        //         'status' => false,
-        //         'message' => $th->getMessage()
-        //     ], 500);
-        // }
         // Validate request data
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
@@ -80,40 +51,6 @@ class AuthController extends Controller
 
     }
     public function loginUser(Request $request){
-    //     try {
-    //         $validateUser = Validator::make($request->all(), [
-    //             'email'=>'required',
-    //             'password'=>'required'
-    //         ]);
-    //         if($validateUser->fails()){
-    //             return response()->json([
-    //                 'status' => false,
-    //                 'message' => 'Validation Error',
-    //                 'errors' => $validateUser->errors()
-    //             ], 401);
-    //         }
-    //         if(!Auth::attempt($request->only(['email', 'password']))){
-    //             return response()->json([
-    //                 'status' => false,
-    //                 'message' => 'Email or Password does not match with our record'
-    //             ], 401);
-    //         }
-    //         $user = User::where('email', $request)->first();
-
-    //         return response()->json([
-    //             'status' => true,
-    //             'message'=> 'User Logged In Successfully',
-    //             'token' => $user->createToken("API TOKEN")->plainTextToken
-    //         ], 200);
-
-    //     } catch (\Throwable $th) {
-    //         return response()->json([
-    //             'status' => false,
-    //             'message' => $th->getMessage()
-    //         ], 500);
-    //     }
-    // }
-
         if (!Auth::attempt($request->only('email', 'password'))) {
                 return response()->json([
                     'message' => 'Invalid login details'
@@ -194,5 +131,27 @@ class AuthController extends Controller
                 'message'=>'password reseted successfully'
             ], 200);
         }
+    }
+    public function changeRole(Request $request, $id){
+        $validatedData = $request->validate([
+            'role' => 'required|in:0,1,2',
+        ]);
+
+        $user = User::findOrFail($id);
+        $user->update($validatedData);
+
+        return response()->json([
+            'status'=>true,
+            'message' => 'User Role changed successfully',
+            'user' => $user
+        ], 201);
+    }
+    public function allUsers(){
+        $users = User::all();
+        return response()->json([
+            'status'=>true,
+            'message'=W'Users List',
+            'Users'=>$users
+        ],200);
     }
 }
